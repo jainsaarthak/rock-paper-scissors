8372c876a87843a25070d4c256ef3bdfd9d90d29
saarthak_300067723 <saarthak.jain@myntra.com>
2019-10-10 13:09:16 +0530

    Refactored game
    Signed-off-by: saarthak_300067723 <saarthak.jain@myntra.com>

--- game.py
+++ game.py
@@ -16,6 +16,6 @@ print(f'the computer chose "{computer_choice}"')
 if user_choice == computer_choice:
     print("It's a tie.")
 elif (user_choice == 'r' and computer_choice == 's') or (user_choice == 'p' and computer_choice == 'r') or (user_choice == 's' and computer_choice == 'p'):
-    print(f'You win! "{user_choice}" beats "{computer_choice}"')
+    print(f'You win! {user_choice} beats {computer_choice}')
 else:
-    print(f'You lose! "{computer_choice}" beats "{user_choice}"')
+    print(f'You lose! {computer_choice} beats {user_choice}')
